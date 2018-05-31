e4e5d2fafd4eebee1588ba48f0eef95d5530fa35
SaulSandovalM <sauldevelop@gmail.com>
2018-05-31 11:36:55 -0500

    keytool y tabs

--- src/components/home/Index.js
+++ src/components/home/Index.js
@@ -5,13 +5,12 @@
 
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, ScrollView, Image, ImageBackground} from 'react-native';
-import {Container, Content, Tab, Tabs} from 'native-base';
+import {Container, Content, Footer, FooterTab, Button, Icon} from 'native-base';
 import img3 from '../../assets/imgs/pasto.jpg';
-import Index from './Index';
 import Stadistics from '../stadistics/Stadistics';
 import Market from '../market/Market';
 
-export default class Home extends Component{
+export default class Index extends Component{
   static navigationOptions = ({ navigation }) => {
     const params = navigation.state.params || {};
 
@@ -46,6 +45,53 @@ export default class Home extends Component{
           </Tab>
         </Tabs>
 
+        <Content>
+
+        <View style={styles.view2}>
+          <View style={styles.margin}>
+            <View style={styles.center}>
+              <Text style={styles.title}>Proximos Partidos</Text>
+            </View>
+
+            <View>
+              <Text style={styles.date}>Lunes, 21 de Mayo</Text>
+            </View>
+
+            <View style={styles.center}>
+              <View style={styles.view}>
+                <Text style={styles.color}>Pachuca</Text>
+              </View>
+
+              <Image style={styles.img} source={{uri:'http://as00.epimg.net/img/comunes/fotos/fichas/equipos/large/4245.png'}}/>
+              <Text style={styles.color}> 7:00 PM </Text>
+              <Image style={styles.img} source={{uri:'http://as00.epimg.net/img/comunes/fotos/fichas/equipos/large/4245.png'}}/>
+
+              <View style={styles.view}>
+                <Text style={styles.color}>Pachuca</Text>
+              </View>
+            </View>
+
+            <View>
+              <Text style={styles.date}>Martes, 22 de Mayo</Text>
+            </View>
+
+            <View style={styles.center}>
+              <View style={styles.view}>
+                <Text style={styles.color}>America</Text>
+              </View>
+
+              <Image style={styles.img} source={{uri:'http://as00.epimg.net/img/comunes/fotos/fichas/equipos/large/4245.png'}}/>
+              <Text style={styles.color}> 9:00 PM </Text>
+              <Image style={styles.img} source={{uri:'http://as00.epimg.net/img/comunes/fotos/fichas/equipos/large/4245.png'}}/>
+
+              <View style={styles.view}>
+                <Text style={styles.color}>Cruz Azul</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+
+        </Content>
       </ImageBackground>
       </Container>
     );
